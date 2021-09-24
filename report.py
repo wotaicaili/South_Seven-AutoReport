ec4e13019123c5db6e800f3de159766141679bad
Pipixia <yukinon@srpr.cc>
2021-09-24 10:11:28 +0800

    Update report.py

--- report.py
+++ report.py
@@ -177,7 +177,7 @@ class Report(object):
             'CAS_LT': cas_lt,
             'LT': lt_code
         }
-        s.post(url, data=data)
+        s.post("https://passport.ustc.edu.cn/login", data=data)
 
         print("login...")
         return s
