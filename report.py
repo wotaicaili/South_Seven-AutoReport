d7e7fef006d75cde9061e418ab8bcb8161e34f49
pipixia244 <yukinon@srpr.cc>
2022-06-04 16:25:37 +0800

    fix bugs; shizukani

--- report.py
+++ report.py
@@ -1,5 +1,9 @@
 #!/bin/python3
-# encoding=utf8 
+# encoding=utf8
+from selenium import webdriver
+from PIL import Image
+import time
+import urllib.parse
 import requests
 import json
 import time
@@ -8,7 +12,7 @@ import pytz
 import re
 import sys
 import argparse
-
+import urllib.parse
 import io
 import os
 from bs4 import BeautifulSoup
@@ -20,14 +24,13 @@ CAS_RETURN_URL = "https://weixine.ustc.edu.cn/2020/caslogin"
 UPLOAD_PAGE_URL = "https://weixine.ustc.edu.cn/2020/upload/xcm"
 UPLOAD_IMAGE_URL = "https://weixine.ustc.edu.cn/2020img/api/upload_for_student"
 UPLOAD_INFO = [
-    (1, "14-day Big Data Trace Card"),
-    (2, "An Kang code"),
+    (1, "14-day Big Data Trace Card")
 ]
 DEFAULT_PIC = ['https://raw.githubusercontent.com/pipixia244/South_Seven-AutoReport/master/14day.jpg', 
                'https://raw.githubusercontent.com/pipixia244/South_Seven-AutoReport/master/ankang.jpg']
 
 class Report(object):
-    def __init__(self, stuid, password, data_path, emer_person, relation, emer_phone, dorm_building, dorm, _14days_pic, ankang_pic):
+    def __init__(self, stuid, password, data_path, emer_person, relation, emer_phone, dorm_building, dorm, _14days_pic, ankang_pic, phone, addr):
         self.stuid = stuid
         self.password = password
         self.data_path = data_path
@@ -36,10 +39,44 @@ class Report(object):
         self.emer_phone = emer_phone
         self.dorm_building = dorm_building
         self.dorm = dorm
-        self.pic= [_14days_pic, ankang_pic]
+        self.pic = [_14days_pic, ankang_pic]
+        self.phone = phone
+        if addr == '':
+            self.addr = urllib.parse.quote("安徽省合肥市")
+        else:
+            self.addr = addr
+        
+    def screenshot(self):
+        phone = self.phone
+        addr = self.addr
+        addr_urlencode = urllib.parse.quote(addr)
+        url = f"https://card.srpr.moe/cn-trip-card/#{phone}&{addr_urlencode}"
+
+        # 创建chrome参数对象
+        opt = webdriver.ChromeOptions()
+
+        opt.headless = True #设置无界面模式，windows开启会出现跨域cookie报错需要关闭，linux可以正常开启，自动适配对应参数
+        opt.add_argument('--no-sandbox')
+        opt.add_argument('--disable-gpu')
+        opt.add_argument('--hide-scrollbars')
+        opt.add_experimental_option('mobileEmulation', {'deviceName': 'Samsung Galaxy S8+'})
+
+
+        driver = webdriver.Chrome(options=opt)#此处填写chromedriver的路径
+        driver.get(url)
+        width = driver.execute_script("return document.documentElement.scrollWidth")
+        height = driver.execute_script("return document.documentElement.scrollHeight")
+        driver.set_window_size(width,height)
+
+        driver.get_screenshot_as_file('webpage.png')
+        print("整个网页尺寸:height={},width={}".format(height,width))
+        im=Image.open('webpage.png')
+        print("截图尺寸:height={},width={}".format(im.size[1],im.size[0]))
+        return im
 
     def report(self):
-        
+        phone = self.phone
+        addr = self.addr
         # 统一验证登录
         loginsuccess = False
         retrycount = 5
@@ -102,7 +139,20 @@ class Report(object):
         # 自动上传健康码
         is_new_upload = 0
         is_user_upload = 0
+        can_upload_file = 0
+        if self.phone != '' and self.addr != '':
+            can_upload_file = 1
+            is_user_upload = 1
+        try:
+            #print(self.phone, self.addr)
+            self.screenshot()
+        except Exception as e:
+            print(e)
+            print('error!')
+            can_upload_file = 0
+        
         if(self.pic[0] != '' and self.pic[1] != ''):
+            can_upload_file = 0
             is_user_upload = 1
         ret = session.get("https://weixine.ustc.edu.cn/2020/apply/daliy/i?t=3")
         if (ret.url == "https://weixine.ustc.edu.cn/2020/upload/xcm" or is_user_upload == 1):
@@ -121,14 +171,20 @@ class Report(object):
                 if(self.pic[idx - 1] == ''):
                     self.pic[idx - 1] = DEFAULT_PIC[idx - 1]
                 #print(self.pic[idx - 1])
-                ret = session.get(self.pic[idx - 1])
-                blob = ret.content
+                if can_upload_file:
+                    with open("webpage.png", 'rb') as f:
+                        pngfile = f.read() 
+                else:
+                    ret = session.get(self.pic[idx - 1])                     
+                if can_upload_file:
+                    blob = pngfile
+                else:
+                    blob = ret.content
                 #print(len(blob))
                 #print(ret.status_code)
                 if blob == None or ret.status_code != 200:
                     print(f"ignore {description}.")
-                    continue        
-
+                    continue                  
                 #print(r.text)
                 r = session.get(UPLOAD_PAGE_URL)
                 #print(r.text)
@@ -168,15 +224,19 @@ class Report(object):
                 "lastModifiedDate": datetime.datetime.now()
                     .strftime("%a %b %d %Y %H:%M:%S GMT+0800 (China Standard Time)"),
                 "size": f"{len(blob)}",
-                }
+                }          
                 payload_files = {"file": (payload["name"], blob)}
                 headers_upload = session.headers
                 headers_upload['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36'
                 r = session.post(url, data=payload, files=payload_files, headers=headers_upload)
                 #print(r)
-                #print(r.text)
+                #print(r.text):
                 r.raise_for_status()
-                print(f"Uploaded {description}: {r.json()['status']}")
+                if can_upload_file:
+                    info = file
+                else:
+                    info = picture
+                print(f"Uploaded {description} {info}: {r.json()['status']}")
             
             
             
@@ -209,18 +269,18 @@ class Report(object):
         }
         ret = session.post(url=REPORT_URL, data=REPORT_DATA)
         
-        #删除占用码(可选功能, 默认关闭, 若想开启请取消注释)
-        if (is_new_upload == 1 and is_user_upload == 0):
-           print("delete.")
-           header = session.headers
-           header['referer'] = "https://weixine.ustc.edu.cn/2020/upload/xcm"
-           header['X-CSRF-TOKEN'] = token2
-           ret1 = session.post("https://weixine.ustc.edu.cn/2020/upload/1/delete", headers=header)
-           ret2 = session.post("https://weixine.ustc.edu.cn/2020/upload/2/delete", headers=header)
-           if(ret1.status_code < 400 and ret2.status_code < 400):
-               print("delete success.")
-           else:
-               print(f"delete error, error code: {ret1} and {ret2}.") 
+        # #删除占用码(可选功能, 默认关闭, 若想开启请取消注释)
+        # if (is_new_upload == 1 and is_user_upload == 0):
+        #    print("delete.")
+        #    header = session.headers
+        #    header['referer'] = "https://weixine.ustc.edu.cn/2020/upload/xcm"
+        #    header['X-CSRF-TOKEN'] = token2
+        #    ret1 = session.post("https://weixine.ustc.edu.cn/2020/upload/1/delete", headers=header)
+        #    ret2 = session.post("https://weixine.ustc.edu.cn/2020/upload/2/delete", headers=header)
+        #    if(ret1.status_code < 400 and ret2.status_code < 400):
+        #        print("delete success.")
+        #    else:
+        #        print(f"delete error, error code: {ret1} and {ret2}.") 
 
         if ret.status_code == 200:
             print("success! code: "+str(ret.status_code))
@@ -286,10 +346,12 @@ if __name__ == "__main__":
     parser.add_argument('dorm', help='dorm number', type=str)
     parser.add_argument('_14days_pic', help='14 days Big Data Trace Card', type=str)
     parser.add_argument('ankang_pic', help='An Kang Health Code', type=str)
+    parser.add_argument('phone', help='phone', type=str)
+    parser.add_argument('addr', help='addr', type=str)
     args = parser.parse_args()
     autorepoter = Report(stuid=args.stuid, password=args.password, data_path=args.data_path, emer_person=args.emer_person, 
                          relation=args.relation, emer_phone=args.emer_phone, dorm_building=args.dorm_building, dorm=args.dorm, 
-                         _14days_pic=args._14days_pic, ankang_pic=args.ankang_pic)
+                         _14days_pic=args._14days_pic, ankang_pic=args.ankang_pic, phone=args.phone, addr=args.addr)
     count = 5
     while count != 0:
         ret = autorepoter.report()
